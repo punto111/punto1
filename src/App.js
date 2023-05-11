7de67e2d973bad14fdf11784ea890b1f9f3272a7
wyf <1017618540@qq.com>
2023-05-11 23:31:59 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-/*
- * @Author: wyf 1299406569@qq.com
- * @Date: 2023-05-03 22:38:51
- * @LastEditors: wyf 1299406569@qq.com
- * @LastEditTime: 2023-05-10 22:31:35
- * @FilePath: \my-app\src\App.js
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- */
-
-import {Demo} from './charts/index'
-import './App.css';
-
-function App() {
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import IndexPage from './pages/index/index';
+import UserPage from './pages/user/index';
+import InfoPage from './pages/info/index';
+function Router() {
   return (
-    <div className="App">
-      <Demo />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<IndexPage />} />
+        <Route path="/user" element={<UserPage />} />
+        <Route path="/info" element={<InfoPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
-export default App;
+export default Router;
---
+++ src/pages/index/index.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import type { MenuProps } from 'antd';
+import { Layout, Menu, theme } from 'antd';
+import './index.css'
+import { Link } from 'react-router-dom';
+const { Header, Content } = Layout;
+
+const items1: MenuProps['items'] = [{
+  key: '1',
+  label: <Link to="/">主页面</Link>,
+},{
+  key: '2',
+  label: <Link to="/user">用户页面</Link>,
+},{
+  key: '3',
+  label:  <Link to="/info">信息页面</Link>,
+}]
+
+
+const IndexPage: React.FC = () => {
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
+  return (
+    <Layout>
+      <Header className="header">
+        <div className="logo" />
+        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']} items={items1} />
+      </Header>
+      <Content
+            style={{
+              padding: 24,
+              margin: 0,
+              minHeight: 280,
+              background: colorBgContainer,
+            }}
+          >
+        主页面
+      </Content>
+    </Layout>
+  );
+};
+
+export default IndexPage;
